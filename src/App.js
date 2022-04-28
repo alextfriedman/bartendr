98c5fd289c007e64ed35c8c14c872ceb5c67d1db
alexandrafriedman <alextfriedman@gmail.com>
2022-04-28 17:25:02 -0400

    initial commit with functional API call

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+// Pseudo code
+// Get cocktail data from API
+// Listen for user selection (from dropdown menu) of which mood they're in
+// Attribute each mood selection to an individual cocktail by adding a class name
+// Display the cocktail to the screen based on user selection
+
+import { useEffect } from "react";
+import axios from "axios";
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    axios({
+      url: "https://www.thecocktaildb.com/api/json/v1/1/search.php",
+      method: "GET",
+      dataResponse: "json",
+      params: {
+        s: "Old Fashioned"
+      },
+    }).then((res) => {
+      console.log(res.data.drinks);
+    })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Bartendr</h1>
+      <p>Select your mood to reveal what cocktail you should make!</p>
+      <form>
+        <select name="mood" id="mood">
+          <option value="initial" disabled selected>How are you feeling?</option>
+          <option value="">Excited</option>
+          <option value="">Content</option>
+          <option value="">Angry</option>
+          <option value="">Depressed</option>
+          <option value="">Nervous</option>
+          <option value="">Peaceful</option>
+          <option value="">Nostalgic</option>
+          <option value="">Romantic</option>
+        </select>
+        <button>Submit</button>
+      </form>
+
+      <p>Questions:</p>
+      <ul>
+        <li>Do I need to make a new API call with a different search param each time the button is clicked? Or can I somehow return an array of all 8 drinks I want on page load?</li>
+      </ul>
     </div>
   );
 }
